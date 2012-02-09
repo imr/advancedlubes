665e058e779a872976e9cd9fd6d65f16f3689c7c
imr <iron_hat@hotmail.com>
2012-02-09 11:42:40 -0500

    superbatch: update tanksheet for dompdf and simple output

--- tanksheet.php
+++ tanksheet.php
@@ -1,53 +1,64 @@
 <?php
 
 require_once dirname(__FILE__) . '/lib/Application.php';
+require_once dirname(__FILE__) . '/dompdf/dompdf_config.inc.php';
 Horde_Registry::appInit('superbatch');
 
 Horde::addScriptFile('tables.js', 'horde');
-Horde::addScriptFile('tooltips.js', 'horde');
-require $registry->get('templates', 'horde') . '/common-header.inc';
-echo Horde::menu();
-$notification->notify(array('listeners' => 'status'));     
 
 $super_driver = $GLOBALS['injector']->getInstance('Superbatch_Factory_Driver')->create();
 $tanks = $super_driver->listTanks();
-$rowOdd = false;
+$rowOdd = 'rowEven';
 
-echo Horde::link(Horde::url('tanksheet.php?view=pdf'), _("View PDF")) . 'View PDF</a><BR><BR>';
-echo Horde::link(Horde::url('tankmeasure.php'), _("Update Inventory")) . 'Update Inventory</a><BR>';
-?>
-<table width="100%" cellspacing=0">
-  <thead>
-    <tr> 
-      <th>Tank</th>
-      <th>Description</th>
-      <th align=right>Max Capacity</th>
-      <th align=right>Previous Volume</th>
-      <th align=right>Previous Measurement</th>
-      <th align=right>New Value</th>
-    </tr>
-  </thead>
-  <tbody>
-<?php
+$vars = Horde_Variables::getDefaultVariables();
+$view = $vars->get('view');
+/*
+if ($view == 'pdf') {
+    try {
+        $wkhtmltopdf = new Wkhtmltopdf(array('path' => './uploads'));
+        $wkhtmltopdf->setTitle("Tank Inventory Sheet");
+        $wkhtmltopdf->setHTML(file_get_contents("http://example.com"));
+        $wkhtmltopdf->output(Wkhtmltopdf::MODE_DOWNLOAD, "tanks.pdf");
+        exit;
+    } catch (Exception $e) {
+        $notification->push($e->getMessage());
+    }
+} */
+$html = '<table width="100%" cellspacing=0><thead><tr><th>Tank</th><th>Description</th>' .
+        '<th align=right>Max Capacity</th><th align=right>Previous Volume</th>' .
+        '<th align=right>Previous Measurement</th><th align=right>New Value</th></tr></thead><tbody>';
 foreach ($tanks as $tank) {
-?>
-    <tr class="<?php echo $rowOdd ? 'rowOdd':'rowEven' ?>">
-      <td rowspan=2 align=center><?php echo $tank['tanknum'] ?></td>
-      <td><?php echo $tank['description'] ?></td>
-      <td rowspan=2 class="rightAlign"><?php echo $tank['capacity'] ?></td>
-      <td rowspan=2 class="rightAlign"><?php echo $tank['Conversion'] * $tank['measured_inches'] ?></td>
-      <td rowspan=2 class="rightAlign"><?php echo $tank['measured_inches'] ?></td>
-      <td>&nbsp;</td>
-    </tr>
-    <tr class="<?php echo $rowOdd ? 'rowOdd':'rowEven' ?>">
-      <td><?php echo $tank['compatibility'] ?></td>
-      <td>&nbsp;</td>
-    </tr>
-<?php
-    $rowOdd = !$rowOdd;
+    $html .= "<tr class='$rowOdd'>" .
+             "<td rowspan=2 align=center>$tank[tanknum]</td>" .
+             "<td>$tank[description]</td>" .
+             "<td rowspan=2 class='rightAlign'>$tank[capacity]</td>" .
+             "<td rowspan=2 class='rightAlign'>" . $tank['Conversion'] * $tank['measured_inches'] . "</td>" .
+             "<td rowspan=2 class='rightAlign'>$tank[measured_inches]</td>" .
+             "<td rowspan=2 style='border-bottom:solid'>&nbsp;</td></tr>" .
+             "<tr class='$rowOdd'>" .
+             "<td>$tank[compatibility]</td></tr>";
+    if ($rowOdd == 'rowOdd') {
+        $rowOdd = 'rowEven';
+    } else {
+        $rowOdd = 'rowOdd';
+    }
 }
-?>
-  </tbody>
-</table>
-<?php
-require $registry->get('templates', 'horde') . '/common-footer.inc';
+$html .= '</tbody></table>';
+if ($view == 'pdf') {
+    $dompdf = new DOMPDF();
+    $dompdf->load_html($html);
+    $dompdf->render();
+    $dompdf->stream("tanks.pdf");
+        echo '<html><body>' . $html;
+} elseif ($view == 'simple') {
+        require $registry->get('templates', 'horde') . '/common-header.inc';
+        echo $html . '</body></html>';
+} else {
+        require $registry->get('templates', 'horde') . '/common-header.inc';
+        echo Horde::menu();
+        $notification->notify(array('listeners' => 'status'));     
+        echo Horde::link(Horde::url('tanksheet.php?view=pdf'), _("View PDF")) . 'View PDF</a><BR><BR>';
+        echo Horde::link(Horde::url('tankmeasure.php'), _("Update Inventory")) . 'Update Inventory</a><BR>';
+        echo $html;
+        require $registry->get('templates', 'horde') . '/common-footer.inc';
+}
