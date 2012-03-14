a7a49882c6145a305c60e59981dd60abc5d33646
imr <iron_hat@hotmail.com>
2012-03-14 11:23:55 -0400

    better print sheet

--- tanksheet.php
+++ tanksheet.php
@@ -15,10 +15,10 @@ $vars = Horde_Variables::getDefaultVariables();
 $view = $vars->get('view');
 
 $html = '<table border="1px solid" width="100%" cellspacing=0 class="striped sortable"><thead><tr><th>Tank</th>' .
-        '<th>Product</th><th colspan=2>Description</th><th>Note</th>' .
+        '<th>Product</th><th colspan=2>Description</th>' .
         '<th align=right>Max Vol</th><th align=right>Conv</th>' .
         '<th align=right>Tap Min</th><th align=right>Volume</th>' .
-        '<th align=right>Inches</th><th align=right>New Value</th></tr></thead><tbody>';
+        '<th align=right>Inches</th><th>Note</th></tr></thead><tbody>';
 foreach ($tanks as $tank) {
     $prev_volume = round($tank['Conversion'] * $tank['measured_inches']);
     $html .= "<tr>" .
@@ -26,13 +26,12 @@ foreach ($tanks as $tank) {
              "<td width='6%'>$tank[userproduct]</td>" .
              "<td width='23%'>$tank[description]</td>" .
              "<td width='18%'>$tank[compatibility]</td>" .
-             "<td width='18%'>$tank[note]</td>" .
              "<td width='4%' class='rightAlign'>$tank[capacity]</td>" .
              "<td width='4%' class='rightAlign'>$tank[Conversion]</td>" .
              "<td width='4%' class='rightAlign'>" . (int) $tank['tap_volume'] .
              "<td width='5%' class='rightAlign'>" . $prev_volume . "</td>" .
              "<td width='5%' class='rightAlign'>$tank[measured_inches]</td>" .
-             "<td width='11%' style='border-bottom:solid'>&nbsp;</td></tr>";
+             "<td width='29%'>$tank[note]</td></tr>";
 }
 if ($view == 'pdf') {
     $dompdf = new DOMPDF();
@@ -47,7 +46,7 @@ if ($view == 'pdf') {
         require $registry->get('templates', 'horde') . '/common-header.inc';
         echo Horde::menu();
         $notification->notify(array('listeners' => 'status'));     
-        echo Horde::link(Horde::url('tanksheet.php?view=simple'), _("View Print Sheet")) . 'View Print Sheet</a> ';
+        echo Horde::link(Horde::url('tanksheet.php?view=simple'), _("Printer Friendly Sheet")) . 'Printer Friendly Sheet</a> ';
         echo Horde::link(Horde::url('tanksheet.php?view=pdf'), _("View PDF")) . 'View PDF</a> ';
         echo Horde::link(Horde::url('tankmeasure.php'), _("Update Inventory")) . 'Update Inventory</a><BR>';
         echo "$html<tr><td>Notes:</td><td colspan=10>$tanknote</td></tr></tbody></table></body></html";
