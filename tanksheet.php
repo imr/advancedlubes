8c3ac2b8a1e9161c080ed2a585249cf32a26a077
imr <iron_hat@hotmail.com>
2012-05-18 14:19:59 -0400

    superbatch: tank sheet read permission.

--- tanksheet.php
+++ tanksheet.php
@@ -12,26 +12,31 @@ $tanknotes = $super_driver->getNote();
 $vars = Horde_Variables::getDefaultVariables();
 $view = $vars->get('view');
 
-$html = '<table border="1px solid" width="100%" cellspacing=0 class="striped sortable"><thead><tr><th>Tank</th>' .
-        '<th>Product</th><th colspan=2>Description</th>' .
-        '<th align=right>Max Vol</th><th align=right>Conv</th>' .
-        '<th align=right>Tap Min</th><th align=right>Inches</th>' .
-        '<th align=right>Volume</th><th>Note</th></tr></thead><tbody>';
-foreach ($tanks as $tank) {
-    $prev_volume = round($tank['Conversion'] * $tank['measured_inches']);
-    $html .= "<tr>" .
-             "<td width='3%'class='leftAlign'>$tank[tanknum]</td>" .
-             "<td width='6%'>$tank[userproduct]</td>" .
-             "<td width='23%'>$tank[description]</td>" .
-             "<td width='18%'>$tank[compatibility]</td>" .
-             "<td width='4%' class='rightAlign'>$tank[capacity]</td>" .
-             "<td width='4%' class='rightAlign'>$tank[Conversion]</td>" .
-             "<td width='4%' class='rightAlign'>" . (int) $tank['tap_volume'] .
-             "<td width='5%' class='rightAlign'>$tank[measured_inches]</td>" .
-             "<td width='5%' class='rightAlign'>" . $prev_volume . "</td>" .
-             "<td width='29%'>$tank[note]</td></tr>";
+if ($GLOBALS['superbatch_perms']->hasPermission('superbatch:tank sheet', $GLOBALS['registry']->getAuth(), Horde_Perms::READ)) {
+    $html = '<table border="1px solid" width="100%" cellspacing=0 class="striped sortable"><thead><tr><th>Tank</th>' .
+            '<th>Product</th><th colspan=2>Description</th>' .
+            '<th align=right>Max Vol</th><th align=right>Conv</th>' .
+            '<th align=right>Tap Min</th><th align=right>Inches</th>' .
+            '<th align=right>Volume</th><th>Note</th></tr></thead><tbody>';
+    foreach ($tanks as $tank) {
+        $prev_volume = round($tank['Conversion'] * $tank['measured_inches']);
+        $html .= "<tr>" .
+                 "<td width='3%'class='leftAlign'>$tank[tanknum]</td>" .
+                 "<td width='6%'>$tank[userproduct]</td>" .
+                 "<td width='23%'>$tank[description]</td>" .
+                 "<td width='18%'>$tank[compatibility]</td>" .
+                 "<td width='4%' class='rightAlign'>$tank[capacity]</td>" .
+                 "<td width='4%' class='rightAlign'>$tank[Conversion]</td>" .
+                 "<td width='4%' class='rightAlign'>" . (int) $tank['tap_volume'] .
+                 "<td width='5%' class='rightAlign'>$tank[measured_inches]</td>" .
+                 "<td width='5%' class='rightAlign'>" . $prev_volume . "</td>" .
+                 "<td width='29%'>$tank[note]</td></tr>";
+    }
+    $html .= "<tr><td>Notes:</td><td colspan=8>$tanknotes[note]</td><td>Revised $tanknotes[date] by $tanknotes[user_id]</td></tr></tbody></table>";
+} else {
+    $html .= "Reading tank sheet not authorized.";
 }
-$html .= "<tr><td>Notes:</td><td colspan=8>$tanknotes[note]</td><td>Revised $tanknotes[date] by $tanknotes[user_id]</td></tr></tbody></table>";
+
 if ($view == 'pdf') {
     $dompdf = new DOMPDF();
     $dompdf->load_html($html);
